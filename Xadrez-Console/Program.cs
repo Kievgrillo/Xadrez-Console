5cf538bed2ec37520625d3c60ef519f70d15f361
Keven Grillo <kevengrillo@gmail.com>
2025-11-24 12:43:57 -0300

    Create class position

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição" + p);
+
+            Console.ReadLine();
+        }
+    }
+}
