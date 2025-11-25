3d2da54501bf4785e9a580cfcf21d7a761b838c3
Keven Grillo <kevengrillo@gmail.com>
2025-11-25 10:12:36 -0300

    start implement tabuleiro

---
+++ Xadrez-Console/Tabuleiro/Cor.cs
@@ -0,0 +1,13 @@
+﻿namespace tabuleiro
+{
+    enum Cor
+    {
+        preto,
+        branco,
+        amarelo,
+        vermelho,
+        azul,
+        verde,
+        laranja
+    }
+}
--- Xadrez-Console/Tabuleiro/Posicao.cs
+++ Xadrez-Console/Tabuleiro/Posicao.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tabuleiro
+﻿namespace tabuleiro
 {
-    public class Posicao
+    class Posicao
     {
         public int Linha { get; set; }
         public int Coluna { get; set; }
