8654a93d10664e77b69e260d75d9c8a4b2073c8c
Keven Grillo <kevengrillo@gmail.com>
2025-11-29 13:06:04 -0300

    Implementation rules the position of the king

--- Xadrez-Console/xadrez/Rei.cs
+++ Xadrez-Console/xadrez/Rei.cs
@@ -11,5 +11,67 @@ namespace xadrez
         {
             return "R";
         }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            // acima
+            pos.DefinirValores(posicao.Linha - 1, posicao.Coluna);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // nordeste
+            pos.DefinirValores(posicao.Linha - 1, posicao.Coluna + 1);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // direita
+            pos.DefinirValores(posicao.Linha, posicao.Coluna + 1);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // sudeste
+            pos.DefinirValores(posicao.Linha + 1, posicao.Coluna + 1);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // abaixo
+            pos.DefinirValores(posicao.Linha + 1, posicao.Coluna);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // sudoeste
+            pos.DefinirValores(posicao.Linha + 1, posicao.Coluna - 1);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // esquerda
+            pos.DefinirValores(posicao.Linha, posicao.Coluna - 1);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // noroeste
+            pos.DefinirValores(posicao.Linha - 1, posicao.Coluna - 1);
+            if (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            return mat;
+        }
     }
 }
